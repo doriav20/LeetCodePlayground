363c4bcd4514cc365bf886e68776d63770ed0c6e
doriav <doriav20@gmail.com>
2025-05-10 20:06:24 +0300

    Implement coloring logic infrastructure

--- colored/colored_cout.cpp
+++ colored/colored_cout.cpp
@@ -3,15 +3,26 @@
 #include <iostream>
 
 namespace colored {
+ColoredCout& ColoredCout::operator<<(const Color new_color) {
+    m_current_color = new_color;
+    return *this;
+}
+
 ColoredCout& ColoredCout::operator<<(std::ostream&(*manip)(std::ostream&)) {
     manip(std::cout);
     return *this;
 }
 
+void ColoredCout::setup_color(const colored::Color color) {}
+
+void ColoredCout::reset_color(const colored::Color color) {}
+
 ColoredCout& ColoredCout::get_instance() {
     static ColoredCout instance;
     return instance;
 }
+
+ColoredCout::ColoredCout() : m_current_color(Color::NO_COLOR) {}
 
 ColoredCout& cout = ColoredCout::get_instance();
 }
--- colored/colored_cout.hpp
+++ colored/colored_cout.hpp
@@ -2,21 +2,38 @@
 
 #include <iostream>
 
+#include "color.hpp"
+
 namespace colored {
 class ColoredCout {
 public:
+    ColoredCout& operator<<(const Color new_color);
+
     ColoredCout& operator<<(std::ostream& (*manip)(std::ostream&));
 
     template<typename T>
     ColoredCout& operator<<(const T& value) {
+        if (m_current_color == Color::NO_COLOR) {
+            std::cout << value;
+            return *this;
+        }
+
+        setup_color(m_current_color);
         std::cout << value;
+        reset_color(m_current_color);
         return *this;
     }
 
     static ColoredCout& get_instance();
 
 private:
-    ColoredCout() = default;
+    ColoredCout();
+
+    static void setup_color(const colored::Color color);
+
+    static void reset_color(const colored::Color color);
+
+    Color m_current_color;
 };
 
 extern ColoredCout& cout;
