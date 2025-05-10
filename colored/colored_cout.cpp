b189efe8807b3b9bb4ed3f8d0265fe91624051c4
doriav <doriav20@gmail.com>
2025-05-10 20:10:50 +0300

    Implement coloring

--- colored/colored_cout.cpp
+++ colored/colored_cout.cpp
@@ -1,6 +1,7 @@
 #include "colored_cout.hpp"
 
 #include <iostream>
+#include <windows.h>
 
 namespace colored {
 ColoredCout& ColoredCout::operator<<(const Color new_color) {
@@ -13,9 +14,38 @@ ColoredCout& ColoredCout::operator<<(std::ostream&(*manip)(std::ostream&)) {
     return *this;
 }
 
-void ColoredCout::setup_color(const colored::Color color) {}
+void ColoredCout::setup_color(const colored::Color color) {
+    const HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
-void ColoredCout::reset_color(const colored::Color color) {}
+    switch (color) {
+        case Color::RED:
+            SetConsoleTextAttribute(console_handle, FOREGROUND_RED);
+            break;
+        case Color::GREEN:
+            SetConsoleTextAttribute(console_handle, FOREGROUND_GREEN);
+            break;
+        case Color::BLUE:
+            SetConsoleTextAttribute(console_handle, FOREGROUND_BLUE);
+            break;
+        case Color::YELLOW:
+            SetConsoleTextAttribute(console_handle, FOREGROUND_RED | FOREGROUND_GREEN);
+            break;
+        case Color::MAGENTA:
+            SetConsoleTextAttribute(console_handle, FOREGROUND_RED | FOREGROUND_BLUE);
+            break;
+        case Color::CYAN:
+            SetConsoleTextAttribute(console_handle, FOREGROUND_GREEN | FOREGROUND_BLUE);
+            break;
+        case Color::WHITE:
+            SetConsoleTextAttribute(console_handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+            break;
+    }
+}
+
+void ColoredCout::reset_color(const colored::Color color) {
+    const HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(console_handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
 
 ColoredCout& ColoredCout::get_instance() {
     static ColoredCout instance;
