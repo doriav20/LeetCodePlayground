bdf98795ca82c48e87096bb5aa931b932c7f38be
doriav <doriav20@gmail.com>
2025-05-10 20:28:09 +0300

    Add support to non-windows platforms

--- colored/colored_cout.cpp
+++ colored/colored_cout.cpp
@@ -1,7 +1,10 @@
 #include "colored_cout.hpp"
 
 #include <iostream>
+
+#ifdef _WIN32
 #include <windows.h>
+#endif
 
 namespace colored {
 ColoredCout& ColoredCout::operator<<(const Color new_color) {
@@ -15,6 +18,7 @@ ColoredCout& ColoredCout::operator<<(std::ostream&(*manip)(std::ostream&)) {
 }
 
 void ColoredCout::setup_color(const colored::Color color) {
+#ifdef _WIN32
     const HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
     switch (color) {
@@ -40,11 +44,40 @@ void ColoredCout::setup_color(const colored::Color color) {
             SetConsoleTextAttribute(console_handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
             break;
     }
+#else
+    switch (color) {
+        case Color::RED:
+            std::cout << "\033[31m";
+            break;
+        case Color::GREEN:
+            std::cout << "\033[32m";
+            break;
+        case Color::BLUE:
+            std::cout << "\033[34m";
+            break;
+        case Color::YELLOW:
+            std::cout << "\033[33m";
+            break;
+        case Color::MAGENTA:
+            std::cout << "\033[35m";
+            break;
+        case Color::CYAN:
+            std::cout << "\033[36m";
+            break;
+        case Color::WHITE:
+            std::cout << "\033[37m";
+            break;
+    }
+#endif
 }
 
 void ColoredCout::reset_color(const colored::Color color) {
+#ifdef _WIN32
     const HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(console_handle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+#else
+    std::cout << "\033[0m";
+#endif
 }
 
 ColoredCout& ColoredCout::get_instance() {
