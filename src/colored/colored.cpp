4e5233bf2fc169f2e9779408b0852296630ff5c8
doriav <doriav20@gmail.com>
2026-09-13 22:56:45 +0300

    Enable escape codes in Windows console

--- src/colored/colored.cpp
+++ src/colored/colored.cpp
@@ -9,6 +9,17 @@
 
 #include <sys/stat.h>
 
+#ifdef _WIN32
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <io.h>
+#include <windows.h>
+#endif
+
 namespace colored::_internal
 {
 namespace
@@ -30,17 +41,36 @@ bool is_redirected_to_file(std::FILE* file)
 #endif
 }
 
+#ifdef _WIN32
+bool enable_escape_codes(std::FILE* file)
+{
+    const HANDLE handle = reinterpret_cast<HANDLE>(_get_osfhandle(_fileno(file)));
+    DWORD mode = 0;
+    if (GetConsoleMode(handle, &mode) == 0)
+    {
+        return true;
+    }
+    return SetConsoleMode(handle, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING) != 0;
+}
+#else
+bool enable_escape_codes(std::FILE*)
+{
+    return true;
+}
+#endif
+
 bool detect_colors(std::FILE* file)
 {
     if (is_env_set("FORCE_COLOR"))
     {
+        enable_escape_codes(file);
         return true;
     }
     if (is_env_set("NO_COLOR"))
     {
         return false;
     }
-    return !is_redirected_to_file(file);
+    return !is_redirected_to_file(file) && enable_escape_codes(file);
 }
 }
 
